271831e3698672913ffc1853cf002c986c78f8c0
Dohyun Jung <serithemage@gmail.com>
2025-06-16 08:59:19 +0900

    chore: 백엔드 개발 환경 설정\n\n- ESLint flat config 설정 추가\n- TypeScript 빌드 설정 수정\n- Jest 테스트 환경 구성

--- backend/bin/app.ts
+++ backend/bin/app.ts
@@ -1,29 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AuthStack } from '../lib/auth/auth-stack';
-import { DatabaseStack } from '../lib/database/database-stack';
-import { LambdaStack } from '../lib/lambda/lambda-stack';
-import { ApiStack } from '../lib/api/api-stack';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const app = new cdk.App();
 
-// 환경 설정
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
-
-// 스택 생성
-const authStack = new AuthStack(app, 'TodoAuthStack', { env });
-const databaseStack = new DatabaseStack(app, 'TodoDatabaseStack', { env });
-const lambdaStack = new LambdaStack(app, 'TodoLambdaStack', {
-  env,
-  userPool: authStack.userPool,
-  todoTable: databaseStack.todoTable,
-});
-const apiStack = new ApiStack(app, 'TodoApiStack', {
-  env,
-  userPool: authStack.userPool,
-  todoLambda: lambdaStack.todoLambda,
-}); 
---
+++ backend/eslint.config.js
@@ -0,0 +1,24 @@
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['eslint.config.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+]; 
